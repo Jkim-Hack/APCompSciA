1a5a146757fbf1553a7b5706a523e2e5febf6b40
Jkim-Hack <johnkim1108@gmail.com>
2017-08-16 23:39:28 -0400

    Added stage and launcher

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,5 +1,16 @@
 package MainPackage;
 
-public class MainClass {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class MainClass extends Application {
+
+	@Override public void start(Stage primaryStage) {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 }
