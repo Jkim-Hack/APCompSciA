3c72f2b8dc1cfd42c519c02a4d979d45d0c1ec6d
johnk <johnk@LAPTOP-OTG65OU0>
2017-08-31 23:22:02 -0400

    a

---
+++ src/Test/BooleanTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		
+		int a =0, b = 1, c =2, m =4;
+		
+		m = Math.min(Math.min(a, c), Math.min(b, c)); 
+		System.out.println(m);
+		
+		
+	}
+	
+}
