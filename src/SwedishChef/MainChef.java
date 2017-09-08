92876b934a214e5a14b63f99dd85fb404c4242ae
johnk <johnk@10.88.17.233>
2017-09-08 10:13:08 -0400

    aaa

---
+++ src/SwedishChef/MainChef.java
@@ -0,0 +1,17 @@
+package SwedishChef;
+
+import java.util.*;
+
+public class MainChef {
+
+	String the = "THE";
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+}
--- src/Test/BooleanTest.java
+++ src/Test/BooleanTest.java
@@ -1,13 +1,19 @@
 package Test;
 
+import java.util.Scanner;
+
 public class BooleanTest {
 
 	public static void main(String[] args) {
 		
 		int a =0, b = 1, c =2, m =4;
 		
-		m = Math.min(Math.min(a, c), Math.min(b, c)); 
-		System.out.println(m);
+		double l = 9.022;
+		
+		System.out.println("You're going           "  + "to \n"
+							 + "ace" + " the test");
+		
+		
 		
 		
 	}
