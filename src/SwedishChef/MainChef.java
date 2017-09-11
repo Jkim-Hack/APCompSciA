69a4723f545079b71fbff0690e3a7488ac8a2ec6
johnk <johnk@LAPTOP-OTG65OU0>
2017-09-11 15:52:27 -0400

    aa

--- src/SwedishChef/MainChef.java
+++ src/SwedishChef/MainChef.java
@@ -10,21 +10,21 @@ public class MainChef {
 		String c = null;
 		String the = "THE";
 		String bork = ". Bork Bork Bork!";
-		//Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Input: ");
-		a = " the an";
+		a = scanner.nextLine().toUpperCase();
 
 		System.out.println(convert(a) + bork.toUpperCase());
 
 	}
 
 	public static String convert(String a) {
-		
-		
+
 		String c = null;
 		String b = null;
 		int length = a.length();
 		int i = length;
+		
 
 		if (a != null) {
 			for (i = length; i >= 1; i = i - 1) {
@@ -41,18 +41,57 @@ public class MainChef {
 
 				}
 			}
-				for (i = length; i >= 1; i = i - 1) {
-					if ((a.substring(i - 1, i)).equalsIgnoreCase("n")) {
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("n")) {
 
-						if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
-							c = a.replace(a.substring(i - 2, i), "UN");
-
-						}
-
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
+						c = b.replace(a.substring(i - 2, i), "UN");
 					}
+
+				}
+				else if ((a.substring(i - 1, i)).equalsIgnoreCase("a")) {
+					c = b.replace(a.substring(i - 1, i), "A");
 				}
 			}
-		return b + c;
+
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("u")) {
+
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
+						c = b.replace(a.substring(i - 2, i), "OO");
+
+					}
+
+				}
+
+			}
+		//Replace all occurances of A and E later
+			
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("w")) {
+
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("o")) {
+						c = b.replace(a.substring(i - 2, i), "OO");
+
+					}
+
+				}
+
+			}
+		/*	
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("O")) {
+ 
+						c = b.replace(a.substring(i - 1, i), "U");
+
+					}
+
+				}
+*/
+			}
+			
+		
+		return c;
 	}
 
 }
