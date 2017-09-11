9a27f2e4acc18685b268ef25487232b35cfffb26
johnk <johnk@10.88.20.49>
2017-09-11 10:23:49 -0400

    aa

--- src/SwedishChef/MainChef.java
+++ src/SwedishChef/MainChef.java
@@ -10,49 +10,49 @@ public class MainChef {
 		String c = null;
 		String the = "THE";
 		String bork = ". Bork Bork Bork!";
-		Scanner scanner = new Scanner(System.in);
+		//Scanner scanner = new Scanner(System.in);
 		System.out.println("Input: ");
-		a = scanner.nextLine();
-		
-		System.out.println(convert(a)  + bork.toUpperCase());
+		a = " the an";
+
+		System.out.println(convert(a) + bork.toUpperCase());
 
 	}
 
 	public static String convert(String a) {
 		
 		
+		String c = null;
 		String b = null;
 		int length = a.length();
 		int i = length;
-		
+
 		if (a != null) {
 			for (i = length; i >= 1; i = i - 1) {
 
 				if ((a.substring(i - 1, i)).equalsIgnoreCase("e")) {
-					
-				if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("h")) {
-						
-				if ((a.substring(i - 3, i - 2)).equalsIgnoreCase("t")) {
-							 b = a.replace(a.substring(i - 3, i), "ZEE");
-							
-					}
-				}
 
-			}
-				
-				if ((a.substring(i - 1, i)).equalsIgnoreCase("n")) {
-				
-				if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
-							b = a.replace(a.substring(i - 2, i), "UN");
-								
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("h")) {
+
+						if ((a.substring(i - 3, i - 2)).equalsIgnoreCase("t")) {
+							b = a.replace(a.substring(i - 3, i), "ZEE");
+
+						}
 					}
 
 				}
 			}
+				for (i = length; i >= 1; i = i - 1) {
+					if ((a.substring(i - 1, i)).equalsIgnoreCase("n")) {
 
-		}
-			return b;
+						if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
+							c = a.replace(a.substring(i - 2, i), "UN");
+
+						}
+
+					}
+				}
+			}
+		return b + c;
 	}
-	
-	
+
 }
