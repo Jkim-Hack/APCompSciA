018da0ce62a2cde0c291dd33a9ec034f5ef74141
stanley <stanley@10.0.1.3>
2017-09-11 21:16:38 -0400

    Some changes to code. At some point, we need to think of a more elegant way to do this. Right now we're doing it by brutal force.

--- src/SwedishChef/MainChef.java
+++ src/SwedishChef/MainChef.java
@@ -13,85 +13,90 @@ public class MainChef {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Input: ");
 		a = scanner.nextLine().toUpperCase();
-
 		System.out.println(convert(a) + bork.toUpperCase());
 
 	}
 
 	public static String convert(String a) {
 
-		String c = null;
 		String b = null;
+		String c = null;
+		String d = null;
+		String e = null;
+		String f = null;
+		String g = null;
+		String h = null;
+		String j = null;
 		int length = a.length();
 		int i = length;
-		
 
 		if (a != null) {
 			for (i = length; i >= 1; i = i - 1) {
-
 				if ((a.substring(i - 1, i)).equalsIgnoreCase("e")) {
-
 					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("h")) {
-
 						if ((a.substring(i - 3, i - 2)).equalsIgnoreCase("t")) {
 							b = a.replace(a.substring(i - 3, i), "ZEE");
-
 						}
 					}
-
 				}
 			}
 			for (i = length; i >= 1; i = i - 1) {
 				if ((a.substring(i - 1, i)).equalsIgnoreCase("n")) {
-
 					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
 						c = b.replace(a.substring(i - 2, i), "UN");
 					}
-
-				}
-				else if ((a.substring(i - 1, i)).equalsIgnoreCase("a")) {
-					c = b.replace(a.substring(i - 1, i), "A");
 				}
 			}
-
 			for (i = length; i >= 1; i = i - 1) {
 				if ((a.substring(i - 1, i)).equalsIgnoreCase("u")) {
-
 					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
-						c = b.replace(a.substring(i - 2, i), "OO");
-
+						d = c.replace(a.substring(i - 2, i), "OO");
 					}
-
 				}
-
 			}
-		//Replace all occurances of A and E later
-			
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("a")) {
+					e = d.replace(a.substring(i - 1, i), "E");
+				}
+			}
 			for (i = length; i >= 1; i = i - 1) {
 				if ((a.substring(i - 1, i)).equalsIgnoreCase("w")) {
-
 					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("o")) {
-						c = b.replace(a.substring(i - 2, i), "OO");
-
+						f = e.replace(a.substring(i - 2, i), "OO");
 					}
-
 				}
-
 			}
-		/*	
-			for (i = length; i >= 1; i = i - 1) {
-				if ((a.substring(i - 1, i)).equalsIgnoreCase("O")) {
- 
-						c = b.replace(a.substring(i - 1, i), "U");
-
+			/*
+			 * for (i = length; i >= 1; i = i - 1) { if ((a.substring(i - 1,
+			 * i)).equalsIgnoreCase("O")) {
+			 * 
+			 * c = b.replace(a.substring(i - 1, i), "U");
+			 * 
+			 * }
+			 * 
+			 * }
+			 */
+		}
+		if (f == null) {
+			if (e == null) {
+				if (d == null) {
+					if (c == null) {
+						if (b == null) {
+							return a;
+						} else {
+							return b;
+						}
+					} else {
+						return c;
 					}
-
+				} else {
+					return d;
 				}
-*/
+			} else {
+				return e;
 			}
-			
-		
-		return c;
+		} else {
+			return f;
+		}
 	}
-
 }
