5134a71963bed4601977f11cbfcbc5ade3131b6e
Jkim-Hack <johnkim1108@gmail.com>
2017-09-13 00:01:48 -0400

    THIS WORKS

--- src/Test/Tester.java
+++ src/Test/Tester.java
@@ -9,18 +9,24 @@ public class Tester {
 		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
 		String b = null;
+		String bork = ". Bork Bork Bork!";
+
+		//add same thing but have an if statement with just 2 characters.
 		
-		for (int i = 0; i <= s.length() - 4 ; i++) {
-		    String substr = s.substring(i, i + 4);
-		    if (substr.matches("(.*)the(.*)")) { System.out.print(" ZEE");  
-		    b = " ZEE";
-		    }
-		    else if(substr.matches("(.*)an(.*)")) {
-		    	b = " UN";
-		    }
+			for (int i = 0; i <= s.length() - 3; i++) {
+				String substr = s.substring(i, i + 3);
+			
+				if (substr.matches("(.*)the(.*)")||substr.matches("(.*)The(.*)")||substr.matches("(.*)THe(.*)")||substr.matches("(.*)THE(.*)")) {
+					s = s.replaceAll("the", "ZEE");
+					
+				} 
+				else if (substr.matches("(.*)an(.*)")||substr.matches("(.*)An(.*)")||substr.matches("(.*)AN(.*)")||substr.matches("(.*)aN(.*)")) {
+					s = s.replaceAll("an", "UN");
+					
+				} 
+			}
+			System.out.print(s.toUpperCase() + bork.toUpperCase());
 		
-		}
-		System.out.println(b);
 	}
 
 }
