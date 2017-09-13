d6f91ce9946be74281119e73d85677adfba7f079
johnk <johnk@LAPTOP-OTG65OU0>
2017-09-12 20:09:54 -0400

    added a lil test thing in the test package

---
+++ src/Test/Tester.java
@@ -0,0 +1,137 @@
+package Test;
+
+import java.util.*;
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+		String a;
+		String c = null;
+		String the = "THE";
+		String bork = ". Bork Bork Bork!";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input: ");
+//		a = scanner.nextLine().toUpperCase();
+		a = "the";
+		System.out.println(convert(a) + bork.toUpperCase());
+
+	}
+
+	public static String convert(String a) {
+
+		String b = null;
+		String c = null;
+		String d = null;
+		String e = null;
+		String f = null;
+		String g = null;
+		String h = null;
+		String j = null;
+		int k = 1;
+		int length = a.length();
+		int i = length;
+
+		if (a != null) {
+			while(k < length) {
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - k, i)).equalsIgnoreCase("e")) {
+					k++;
+					if ((a.substring(i - k, i - k)).equalsIgnoreCase("h")) {
+						k++;
+						if ((a.substring(i - k, i - k)).equalsIgnoreCase("t")) {
+							k++;
+							b = a.replace(a.substring(i - k, i), "ZEE");
+						}
+					}
+				}
+			}
+			return b;
+			}
+			/*
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("n")) {
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
+						c = b.replace(a.substring(i - 2, i), "UN");
+					}
+				}
+			}
+			
+			
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("u")) {
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("a")) {
+						d = c.replace(a.substring(i - 2, i), "OO");
+					}
+				}
+			}
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("a")) {
+					d = c.replace(a.substring(i - 1, i), "E");
+				}
+			}
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("w")) {
+					if ((a.substring(i - 2, i - 1)).equalsIgnoreCase("o")) {
+						e = d.replace(a.substring(i - 2, i), "OO");
+					}
+				}
+			}
+
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("O")) {
+
+					f = e.replace(a.substring(i - 1, i), "U");
+
+				}
+
+			}
+			for (i = length; i >= 1; i = i - 1) {
+				if ((a.substring(i - 1, i)).equalsIgnoreCase("R")) {
+					if ((a.substring(i - 2, i)).equalsIgnoreCase("I")) {
+						g = f.replace(a.substring(i - 2, i), "UR");
+					}
+				}
+
+			}
+			if (j == null) {
+				if (h == null) {
+					if (g == null) {
+						if (f == null) {
+							if (e == null) {
+								if (d == null) {
+									if (c == null) {
+										if (b == null) {
+											return a;
+										} else {
+											return b;
+										}
+									} else {
+										return c;
+									}
+								} else {
+									return d;
+								}
+							} else {
+								return e;
+							}
+						} else {
+							return f;
+						}
+					} else {
+						return g;
+					}
+				} else {
+					return h;
+				}
+			} else {
+				return j;
+			}
+			*/
+		}
+			return b;
+	}
+	
+}
+	
+
