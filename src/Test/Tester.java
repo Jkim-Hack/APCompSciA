cdc6e4fe55a7f95be30c2b74360fe7b428de4dbe
Jkim-Hack <johnkim1108@gmail.com>
2017-09-13 00:46:16 -0400

    This works

--- src/Test/Tester.java
+++ src/Test/Tester.java
@@ -14,14 +14,18 @@ public class Tester {
 		//add same thing but have an if statement with just 2 characters.
 		
 			for (int i = 0; i <= s.length() - 3; i++) {
-				String substr = s.substring(i, i + 3);
+				String str = s.substring(i, i + 3);
 			
-				if (substr.matches("(.*)the(.*)")||substr.matches("(.*)The(.*)")||substr.matches("(.*)THe(.*)")||substr.matches("(.*)THE(.*)")) {
+				if (str.matches("(.*)the(.*)")||str.matches("(.*)The(.*)")||str.matches("(.*)THe(.*)")||str.matches("(.*)THE(.*)")) {
 					s = s.replaceAll("the", "ZEE");
 					
 				} 
-				else if (substr.matches("(.*)an(.*)")||substr.matches("(.*)An(.*)")||substr.matches("(.*)AN(.*)")||substr.matches("(.*)aN(.*)")) {
+				else if (str.matches("(.*)an(.*)")||str.matches("(.*)An(.*)")||str.matches("(.*)AN(.*)")||str.matches("(.*)aN(.*)")) {
 					s = s.replaceAll("an", "UN");
+					
+				} 
+				else if (str.matches("(.*)au(.*)")||str.matches("(.*)Au(.*)")||str.matches("(.*)AU(.*)")||str.matches("(.*)aU(.*)")) {
+					s = s.replaceAll("au", "OO");
 					
 				} 
 			}
