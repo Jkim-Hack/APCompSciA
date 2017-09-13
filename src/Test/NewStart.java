dfc41634d8c63e800b84b7506d70bddadd5a0a1b
stanley <stanley@10.0.1.3>
2017-09-12 22:24:36 -0400

    IndexOf method

--- src/Test/NewStart.java
+++ src/Test/NewStart.java
@@ -1,5 +1,31 @@
 package Test;
 
+import java.util.Scanner;
+
 public class NewStart {
-String something = "something";
+	public static void main(String[] args) {
+
+		String a;
+		String c = null;
+		String the = "THE";
+		String bork = ". Bork Bork Bork!";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input: ");
+		a = scanner.nextLine().toUpperCase();
+		System.out.println(convert(a) + bork);
+	}
+
+	public static String convert(String a) {
+		String b = null;
+		String c = null;
+		String d = null;
+		String e = null;
+		String f = null;
+		String g = null;
+		String h = null;
+		String j = null;
+		int i = a.indexOf("THE");
+		b = a.replace(a.substring(i, i + 3), "ZEE");
+		return b;
+	}
 }
