773714c7523c5249c0205a80245d88a336004bc0
johnk <johnk@10.88.20.126>
2017-09-13 10:51:26 -0400

    Latest code

--- src/Test/Tester.java
+++ src/Test/Tester.java
@@ -11,22 +11,64 @@ public class Tester {
 		String b = null;
 		String bork = ". Bork Bork Bork!";
 
-		//add same thing but have an if statement with just 2 characters.
-		
-			for (int i = 0; i <= s.length() - 3; i++) {
-				String substr = s.substring(i, i + 3);
-			
-				if (substr.matches("(.*)the(.*)")||substr.matches("(.*)The(.*)")||substr.matches("(.*)THe(.*)")||substr.matches("(.*)THE(.*)")) {
-					s = s.replaceAll("the", "ZEE");
-					
-				} 
-				else if (substr.matches("(.*)an(.*)")||substr.matches("(.*)An(.*)")||substr.matches("(.*)AN(.*)")||substr.matches("(.*)aN(.*)")) {
-					s = s.replaceAll("an", "UN");
-					
-				} 
-			}
-			System.out.print(s.toUpperCase() + bork.toUpperCase());
-		
-	}
+		// add same thing but have an if statement with just 2 characters.
+		for (int i = 0; i <= s.length() - 3; i++) {
+			String substr = s.substring(i, i + 3).toUpperCase();
+			if (s.length() == 1) {
+				if (substr.matches("(.*)A(.*)")) {
+					if (substr.indexOf("A") != substr.length() - 1) {
+						s = s.replaceAll("A", "E");
+					} else if (substr.matches("(.*)O(.*)")) {
+						s = s.replaceAll("O", "U");
+						if (s.length() == 2) {
+							if (substr.matches("(.*)AN(.*)")) {
+								s = s.replaceAll("AN", "UN");
+							} else if (substr.matches("(.*)AU(.*)")) {
+								s = s.replaceAll("AU", "OO");
+							} else if (substr.matches("(.*)OW(.*)")) {
+								s = s.replaceAll("OW", "OO");
+							} else if (substr.matches("(.*)IR(.*)")) {
+								s = s.replaceAll("IR", "UR");
+							} else if (substr.matches("(.*)O(.*)")) {
+								s = s.replaceAll("O", "U");
+							}
 
+						} else if (s.length() == 3) {
+
+						} else if (s.length() == 4) {
+
+						}
+					}
+
+					// if (substr.matches("(.*)THE(.*)")) {
+					// s = s.replaceAll("THE", "ZEE");
+					//
+					// }
+					// }
+					// }
+					// }
+					// }
+					// }
+					// } else if (substr.matches("(.*)TION(.*)")) {
+					// s = s.replaceAll("TION", "SHUN");
+					// } else if (substr.matches("(.*)OW(.*)")) {
+					// s = s.replaceAll("OW", "OO");
+					// } else if (substr.matches("(.*)OW(.*)")) {
+					// s = s.replaceAll("OW", "OO");
+					// } else if (substr.matches("(.*)OW(.*)")) {
+					// s = s.replaceAll("OW", "OO");
+					// } else if (substr.matches("(.*)OW(.*)")) {
+					// s = s.replaceAll("OW", "OO");
+					// } else if (substr.matches("(.*)OW(.*)")) {
+					// s = s.replaceAll("OW", "OO");
+					// } }
+					// }
+					// System.out.print(s.toUpperCase() + bork.toUpperCase());
+					//
+					// }
+
+				}
+			}
+		}
+	}
 }
