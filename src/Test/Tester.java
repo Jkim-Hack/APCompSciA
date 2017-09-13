ca8890f2faac949cba27505d05e7d16e6b459e37
johnk <johnk@10.88.20.126>
2017-09-13 18:10:36 -0400

    Aa

--- src/Test/Tester.java
+++ src/Test/Tester.java
@@ -8,17 +8,175 @@ public class Tester {
 		System.out.println("Print: ");
 		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
-		String b = null;
 		String bork = ". Bork Bork Bork!";
 
-		// add same thing but have an if statement with just 2 characters.
+		//If only one character added
+		if (s.length() == 1) {
+			for (int i = 0; i <= s.length() - 1; i++) {
+				String substr = s.substring(i, i + 1).toUpperCase();
+				
+				//Work on this one vvvv
+				/*
+				if(substr.matches("(.*)A(.*)")) {
+					s = s.replaceAll("A", "E");
+				}
+				*/
+				
+				/*Work on this vvv
+				if(substr.matches("(.*)O(.*)")) {
+					s = s.replace('O', 'U');
+				}
+				*/
+				//These cannot go from one letter string into two
+				/*
+				if(substr.matches("(.*)F(.*)")) {
+					s = s.replaceAll("F", "FF");
+				}
+				else if(substr.matches("(.*)V(.*)")) {
+					s = s.replaceAll("V", "FF");
+				}
+				*/
+				//except this one
+				if(substr.matches("(.*)W(.*)")) {
+					s = s.replace('W', 'V');
+				}
+			}
+		}
+		//If only 2 characters added
+		else if (s.length() == 2) {
+			for (int i = 0; i <= s.length() - 2; i++) {
+				String substr = s.substring(i, i + 2).toUpperCase();
+				
+				if(substr.matches("(.*)AN(.*)")) {
+					s = s.replaceAll("AN", "UN");
+				}
+				else if(substr.matches("(.*)AU(.*)")) {
+					s = s.replaceAll("AU", "OO");
+				}
+				//Work on this one vvvv
+			
+				else if(substr.matches("(.*)A(.*)")) {
+					s = s.replaceAll("A", "E");
+				}
+			
+				else if(substr.matches("(.*)OW(.*)")) {
+					s = s.replaceAll("OW", "OO");
+				}
+				
+				if(substr.matches("(.*)O(.*)")) {
+					s = s.replace('O', 'U');
+				}
+				
+				else if(substr.matches("(.*)IR(.*)")) {
+					s = s.replaceAll("IR", "UR");
+				}
+				
+				else if(substr.matches("(.*)EN(.*)")) {
+					s = s.replaceAll("EN", "EE");
+				}
+				/*
+				else if(substr.matches("(.*)F(.*)")) {
+					s = s.replaceAll("F", "FF");
+				}
+				else if(substr.matches("(.*)V(.*)")) {
+					s = s.replaceAll("V", "FF");
+				}
+				*/
+				else if(substr.matches("(.*)W(.*)")) {
+					s = s.replace('W', 'V');
+				}
+			}
+		}
+	//If only 3 characters added
+	else if (s.length() == 3) {
 		for (int i = 0; i <= s.length() - 3; i++) {
 			String substr = s.substring(i, i + 3).toUpperCase();
+			
 			if (substr.matches("(.*)THE(.*)")) {
 				s = s.replaceAll("THE", "ZEE");
 			}
-			if(substr.matches("(.*)AN(.*)")) {
+			else if(substr.matches("(.*)AN(.*)")) {
 				s = s.replaceAll("AN", "UN");
+			}
+			else if(substr.matches("(.*)AU(.*)")) {
+				s = s.replaceAll("AU", "OO");
+			}
+			//Work on this one vvvv
+			/*
+			if(substr.matches("(.*)A(.*)")) {
+				s = s.replaceAll("A", "E");
+			}
+			*/
+			else if(substr.matches("(.*)OW(.*)")) {
+				s = s.replaceAll("OW", "OO");
+			}
+			
+			if(substr.matches("(.*)O(.*)")) {
+				s = s.replace('O', 'U');
+			}
+			
+			else if(substr.matches("(.*)IR(.*)")) {
+				s = s.replaceAll("IR", "UR");
+			}
+			/*
+			else if(substr.matches("(.*)F(.*)")) {
+				s = s.replaceAll("F", "FF");
+			}
+			else if(substr.matches("(.*)V(.*)")) {
+				s = s.replaceAll("V", "FF");
+			}
+			*/
+			else if(substr.matches("(.*)W(.*)")) {
+				s = s.replace('W', 'V');
+			}
+		}
+	}
+	//If only 4 characters and above added
+	else if(s.length() >= 4) {
+		for (int i = 0; i <= s.length() - 4; i++) {
+			String substr = s.substring(i, i + 4).toUpperCase();
+			
+			if (substr.matches("(.*)THE(.*)")) {
+				s = s.replaceAll("THE", "ZEE");
+			}
+			else if(substr.matches("(.*)AN(.*)")) {
+				s = s.replaceAll("AN", "UN");
+			}
+			else if(substr.matches("(.*)AU(.*)")) {
+				s = s.replaceAll("AU", "OO");
+			}
+			//Work on this one vvvv
+			/*
+			if(substr.matches("(.*)A(.*)")) {
+				s = s.replaceAll("A", "E");
+			}
+			*/
+			else if(substr.matches("(.*)OW(.*)")) {
+				s = s.replaceAll("OW", "OO");
+			}
+			
+			if(substr.matches("(.*)O(.*)")) {
+				s = s.replace('O', 'U');
+			}
+			
+			else if(substr.matches("(.*)IR(.*)")) {
+				s = s.replaceAll("IR", "UR");
+			}
+			else if(substr.matches("(.*)TION(.*)")) {
+				s = s.replaceAll("TION", "SHUN");
+			}
+			/*
+			else if(substr.matches("(.*)F(.*)")) {
+				s = s.replaceAll("F", "FF");
+			}
+			else if(substr.matches("(.*)V(.*)")) {
+				s = s.replaceAll("V", "FF");
+			}
+			*/
+			else if(substr.matches("(.*)W(.*)")) {
+				s = s.replace('W', 'V');
+			}
+			
 			}
 		}
 		System.out.println(s.toUpperCase() + bork.toUpperCase());
