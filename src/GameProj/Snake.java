2a3b6eeef2f7ed5df94bba2a4450ba123afee227
johnk <johnk@10.84.36.72>
2018-01-30 10:39:32 -0500

    aa

--- src/GameProj/Snake.java
+++ src/GameProj/Snake.java
@@ -13,95 +13,82 @@ public class Snake {
 	private Rectangle body;
 	private int cordX;
 	private int cordY;
-	
+
 	public Snake(int pix, Rectangle bod) {
 		snakePix = new ArrayList<Rectangle>(3);
 		body = bod;
-		
-		cordX = 300;
-		cordY = 400;
-		
-		for(int i = 0; i < pix; i++) {
+
+
+		for (int i = 0; i < pix; i++) {
 			snakePix.add(i, bod);
-			snakePix.get(i).setY(cordY + i);
-			snakePix.get(i).setY(cordX + i);
 		}
-		
-		
-		
-		
+
 	}
+
 	public void setSnake() {
-		for(int i = 1; i <= snakePix.size(); i++) {
-			
+		for (int i = 1; i <= snakePix.size(); i++) {
+
 		}
 	}
+
 	public void moveForward() {
 		while (true) {
-			
+
 		}
 	}
-	
+
 	public void moveUp() {
-		
-		for(int i = 0; i < snakePix.size(); i++) {
-			
-			
-			
+
+		for (int i = 0; i < snakePix.size(); i++) {
+
 		}
-		
+
 	}
-	
+
 	public void moveDown() {
-		
+
 	}
 
 	public void moveRight() {
-		
-		int i = 0;
-		
-		while(i < 600) {
-			
-			i += 20;
-			
-				snakePix.get(i).setY(cordY + i);
-				snakePix.get(i).setY(cordX + i);
-			
-			
-		}
-		
-		
+
+		snakePix.get(0).setTranslateX(cordX);
+	
 	}
 
 	public void moveLeft() {
-	
+
 	}
+
 	public ArrayList<Rectangle> getSnakePix() {
 		return snakePix;
 	}
+
 	public void setSnakePix(ArrayList<Rectangle> snakePix) {
 		this.snakePix = snakePix;
 	}
-	
+
 	public Rectangle getBody() {
 		return body;
 	}
+
 	public void setBody(Rectangle body) {
 		this.body = body;
 	}
+
 	public int getCordX() {
 		return cordX;
 	}
+
 	public void setCordX(int cordX) {
 		this.cordX = cordX;
 	}
+
 	public int getCordY() {
 		return cordY;
 	}
+
 	public void setCordY(int cordY) {
 		this.cordY = cordY;
 	}
-	
-	
-	
+
 }
--- src/GameProj/SnakeMain.java
+++ src/GameProj/SnakeMain.java
@@ -1,5 +1,6 @@
 package GameProj;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,39 +13,95 @@ import javafx.stage.Stage;
 
 public class SnakeMain extends Application {
 
-	
+	public int addX = 0;
+	public int addY = 0;
 	public BorderPane pane = new BorderPane();
-	
-	@Override public void start(Stage primaryStage) throws Exception {
-		
+	public boolean moveRight = false;
+	public boolean moveLeft = false;
+	public boolean moveUp = false;
+	public boolean moveDown = false;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
 		pane = new BorderPane();
-		
-		Rectangle rect = new Rectangle(50, 50, Color.GREEN);
-		
-		Snake snake = new Snake(3,rect);
-		
+
+		Rectangle rect = new Rectangle(10, 10, Color.GREEN);
+
+		Snake snake = new Snake(3, rect);
+
 		pane.setCenter(snake.getSnakePix().get(0));
-		
+
 		Scene scene = new Scene(pane, 800, 600);
 		scene.setFill(Color.BLACK);
-		
+
 		scene.setOnKeyPressed(e -> {
-			if(e.getCode() == KeyCode.RIGHT) {
+		
+				if (e.getCode() == KeyCode.RIGHT) {
+					moveRight = true;
+					moveLeft = false;
+					moveUp = false;
+					moveDown = false;
+				}
+				if (e.getCode() == KeyCode.LEFT) {
+					moveRight = false;
+					moveLeft = true;
+					moveUp = false;
+					moveDown = false;
+				}
+				if (e.getCode() == KeyCode.UP) {
+					moveRight = false;
+					moveLeft = false;
+					moveUp = true;
+					moveDown = false;
+				}
+				if (e.getCode() == KeyCode.DOWN) {
+					moveRight = false;
+					moveLeft = false;
+					moveUp = false;
+					moveDown = true;
+				}
+			
+				
+		});
+
+		AnimationTimer animate = new AnimationTimer() {
+
+			@Override
+			public void handle(long now) {
+			if(moveRight) {
+				addX+=1;
+				snake.setCordX(addX);
 				snake.moveRight();
 			}
-			
-			
-		});
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
+			if(moveLeft) {
+				addX-=1;
+				snake.setCordX(addX);
+				snake.moveRight();
+			}
+			if(moveUp) {
+				addY+=1;
+				addX = 0;
+				snake.setCordX(addX);
+				snake.moveRight();
+			}
+			if(moveDown) {
+				addX = 0;
+				addY -= 1;
+				snake.setCordX(addX);
+				snake.moveRight();
+			}
+
+		}
+
+	};animate.start();
+
+	primaryStage.setScene(scene);primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
+
 }
