d4af6e4de587a796ff1c2cfbd1d1de942b91cd80
nisha <nishaajit@hotmail.com>
2018-02-05 10:08:01 -0500

    aa

--- src/GameProj/SnakeMain.java
+++ src/GameProj/SnakeMain.java
@@ -12,12 +12,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-<<<<<<< HEAD
-<<<<<<< HEAD
-import javafx.scene.image.Image; 
-=======
 import javafx.scene.image.Image;
->>>>>>> branch 'master' of https://github.com/Jkim-Hack/APCompSciA.git
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
